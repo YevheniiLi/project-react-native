d18866821a93fdb8f4581c544de7354e30b049c4
Yevhenii Lymarenko <96422034+YevheniiLi@users.noreply.github.com>
2023-08-16 12:45:55 +0200

    added RN components

--- App.js
+++ App.js
@@ -1,20 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  Button,
+  ImageBackground,
+  Image,
+  Text,
+} from "react-native";
+
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  // const nameHandler = (text) => setName(text);
+  // const passwordHandler = (text) => setPassword(text);
+
+  const onLogin = () => {
+    Alert.alert("Credentials", `${name} + ${password}`);
+  };
   return (
+    // <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        style={styles.image}
+        source={ require ('./images/mainbg.png') }
+      >
+        {/* <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+      > */}
+        <View style={styles.form}>
+          <View>
+            <Text style={styles.inputTitle}>LOGIN</Text>
+            <TextInput style={styles.input} textAlign="center" />
+          </View>
+          <View style={{ marginTop: 20}}>
+            <Text style={styles.inputTitle}>EMAIL ADRESS</Text>
+            <TextInput style={styles.input} textAlign="center" />
+          </View>
+          <View style={{ marginTop: 20, marginBottom: 45}}>
+            <Text style={styles.inputTitle}>PASSWORD</Text>
+            <TextInput style={styles.input} 
+            textAlign="center" 
+            secureTextEntry={true} 
+            />
+          </View>
+        </View>
+
+        {/* <TextInput
+          value={password}
+          placeholder="Password"
+          onChangeText={passwordHandler}
+          style={styles.input}
+        /> */}
+        <Button title={"Login"} style={styles.input} onPress={onLogin} />
+        {/* </KeyboardAvoidingView> */}
+      </ImageBackground>
     </View>
+    // </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 450,
+    height: 1000,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#f0f8ff",
+    borderRadius: 6,
+    height: 40,
+    width: 300,
+  },
+  form: {
+    marginHorizontal: 40, 
+  },
+  inputTitle: {
+    color: '#f0f8ff',
+    marginBottom: 10,
+    fontSize: 18,
+  }
 });
